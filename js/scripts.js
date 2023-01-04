bce09b55a1c7e75e2129e5bf4ec1fb001f571c79
brookee10 <bmbeihoff0672@gmail.com>
2023-01-03 19:45:22 -0500

    added pokemon list

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name: "Squirtle", height: .5, types:['water']},
+    {name: "Eevee", height: .3, types:['normal']},
+    {name: "Jigglypuff", height: .5, types:['fairy','normal']}
+]
