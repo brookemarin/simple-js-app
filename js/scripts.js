9e181b7a2b72ff24aaf760a876abd3352db42c9b
brookee10 <bmbeihoff0672@gmail.com>
2023-01-08 14:24:53 -0500

    added for loop to print list of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ let pokemonList = [
     {name: "Eevee", height: .3, types:['normal']},
     {name: "Jigglypuff", height: .5, types:['fairy','normal']}
 ]
+
+
+for (i = 0;i < pokemonList.length;i++){
+    if(pokemonList[i].height >=.5){
+        document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!</p>") 
+    } else {
+      document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")</p>")
+    }
+}
