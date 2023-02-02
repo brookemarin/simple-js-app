f26426040ca71ddd699fc9a4377ef11ebdb62046
brookee10 <bmbeihoff0672@gmail.com>
2023-02-02 11:26:19 -0500

    added IIFE and updated forEach for printing pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
-let pokemonList = [
+let pokemonRepository = (function (){
+    let pokemonList = [
     {name: "Squirtle", height: .5, types:['water']},
     {name: "Eevee", height: .3, types:['normal']},
     {name: "Jigglypuff", height: .5, types:['fairy','normal']}
 ]
+    function getAll(){
+        return pokemonList;
+    }
 
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add, add
+    };
+
+
+})();
 
 // for (i = 0;i < pokemonList.length;i++){
 //     if(pokemonList[i].height >=.5){
@@ -13,4 +28,4 @@ let pokemonList = [
 //     }
 // }
 
-pokemonList.forEach(pokemon => document.write(`<p> name: ${pokemon.name}, height: ${pokemon.height}, types: ${pokemon.types} </p>`));
+pokemonRepository.getAll().forEach(pokemon => document.write(`<p> name: ${pokemon.name}, height: ${pokemon.height}, types: ${pokemon.types} </p>`));
