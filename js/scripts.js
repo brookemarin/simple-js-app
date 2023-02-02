aa01d75f2f2a49c29210afee005ab90aa1f07486
brookee10 <bmbeihoff0672@gmail.com>
2023-02-02 10:34:01 -0500

    added function to print pokemon details

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,12 @@ let pokemonList = [
 ]
 
 
-for (i = 0;i < pokemonList.length;i++){
-    if(pokemonList[i].height >=.5){
-        document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!</p>") 
-    } else {
-      document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")</p>")
-    }
-}
+// for (i = 0;i < pokemonList.length;i++){
+//     if(pokemonList[i].height >=.5){
+//         document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!</p>") 
+//     } else {
+//       document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")</p>")
+//     }
+// }
+
+pokemonList.forEach(pokemon => document.write(pokemon));
